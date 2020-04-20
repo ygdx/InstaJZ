b001192674d0a8b171c8d2126ad467e57ff976e2
Di Yu <diyu@fb.com>
2020-04-20 03:00:48 -0700

    CRUD implemented, course 4 finished

--- Insta/models.py
+++ Insta/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from imagekit.models import ProcessedImageField
 
+from django.urls import reverse
+
 # Create your models here.
 class Post(models.Model):
     title = models.TextField(blank=True, null=True)
@@ -11,3 +13,6 @@ class Post(models.Model):
         blank=True,
         null=True
     )
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
--- Insta/views.py
+++ Insta/views.py
@@ -1,8 +1,30 @@
-# from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title']
+    template_name = 'post_update.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
